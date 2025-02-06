6febc262a2a5c05ab95889117c213bab5dafe2a4
abhigpta19 <abhigpta19@gmail.com>
2025-02-06 10:09:46 +0530

    hoisting done

---
+++ hoisting/index.js
@@ -0,0 +1,21 @@
+
+
+console.log(x);
+console.log(fun1);
+console.log(fun2);
+
+fun1();
+console.log(x);
+// fun2();
+
+var x = 5;
+
+function fun1()
+{
+    console.log("helloword");
+    x=10;
+}
+
+var fun2 = ()=>{
+    console.log("hello2");
+}
